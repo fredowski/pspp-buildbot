4b40195d978813b82100b1778873b4e6b154f4ac
Friedrich Beckmann <friedrich.beckmann@hs-augsburg.de>
2020-08-18 19:49:42 +0200

    update the repository when creating a vm

--- create_debian.sh
+++ create_debian.sh
@@ -29,6 +29,8 @@ lxc-create -n $name -t download -f lxc-config -- -d $distribution -r $release -a
 systemd-run --user -p "Delegate=yes" lxc-start -F $name
 # wait for network
 sleep 15
+lxc-attach -n $name -- apt update
+lxc-attach -n $name -- apt upgrade -y
 lxc-attach -n $name -- apt install -y openssl openssh-server sudo
 lxc-attach -n $name -- /usr/sbin/useradd -s /bin/bash -m -p $(openssl passwd -1 pspp4you) pspp
 lxc-attach -n $name -- sh -c "echo 'pspp ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
