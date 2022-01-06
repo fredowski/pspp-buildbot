6c88b11f61213ea484e866bf32faf50b3f0ca57d
Friedrich Beckmann <friedrich.beckmann@hs-augsburg.de>
2022-01-07 00:57:44 +0100

    update spread-sheet-widget from version 0.7 to version 0.8

--- ci/buildssw.sh
+++ ci/buildssw.sh
@@ -2,14 +2,14 @@
 
 # Download and install spread-sheet-widget
 
-# Copyright (C) 2020 Free Software Foundation, Inc.
+# Copyright (C) 2020,2022 Free Software Foundation, Inc.
 # Released under GNU General Public License, either version 3
 # or any later option
 
 workdir=`pwd`
 
 #Download and install spread-sheet-widget
-sswversion=0.7
+sswversion=0.8
 curl -o ssw.tgz http://alpha.gnu.org/gnu/ssw/spread-sheet-widget-$sswversion.tar.gz
 tar -xzf ssw.tgz
 cd spread-sheet-widget-$sswversion
