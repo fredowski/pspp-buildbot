f794f65a7abb7f67cd3b63340b8c3660e6417883
Friedrich Beckmann <friedrich.beckmann@hs-augsburg.de>
2022-03-14 08:43:05 +0100

    update gnulib version

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -7,7 +7,7 @@
 # or any later option
 
 #Download gnulib
-gnulibver=0edaafc813caff4101c58405c6ab279597afc0b9
+gnulibver=2d830e4a792fcd9f614ed08a7f18584b8b21d23b
 curl -o gnulib.zip https://codeload.github.com/coreutils/gnulib/zip/$gnulibver
 unzip -q gnulib.zip
 rm gnulib.zip
