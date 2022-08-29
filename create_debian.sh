c0ab651bab54abf0ab6ae59ea60083c7668a0a2e
Friedrich Beckmann <friedrich.beckmann@hs-augsburg.de>
2022-08-29 22:04:28 +0200

    add librsvg2-bin package to provide rsvg-convert / change unpriv container
    I added the package because pspp now builds from git using rsvg-convert instead
    using gimp. I keep gimp for the moment to build the stable release. In addition
    debian 11 now uses lxc-unpriv-attach instead of lxc-attach for unpriviliged containers.

--- create_debian.sh
+++ create_debian.sh
@@ -29,20 +29,20 @@ lxc-create -n $name -t download -f lxc-config -- --keyserver keyserver.ubuntu.co
 systemd-run --user -p "Delegate=yes" lxc-start -F $name
 # wait for network
 sleep 15
-lxc-attach -n $name -- apt update
-lxc-attach -n $name -- apt upgrade -y
-lxc-attach -n $name -- apt install -y openssl openssh-server sudo
-lxc-attach -n $name -- /usr/sbin/useradd -s /bin/bash -m -p $(openssl passwd -1 pspp4you) pspp
-lxc-attach -n $name -- sh -c "echo 'pspp ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
-lxc-attach -n $name -- sh -c "echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config"
-lxc-attach -n $name -- su pspp -c 'mkdir ~/.ssh'
-cat buildbot_rsa.pub | lxc-attach -n $name -- /bin/bash -c "/bin/cat > /home/pspp/.ssh/authorized_keys"
+lxc-unpriv-attach -n $name -- apt update
+lxc-unpriv-attach -n $name -- apt upgrade -y
+lxc-unpriv-attach -n $name -- apt install -y openssl openssh-server sudo
+lxc-unpriv-attach -n $name -- /usr/sbin/useradd -s /bin/bash -m -p $(openssl passwd -1 pspp4you) pspp
+lxc-unpriv-attach -n $name -- sh -c "echo 'pspp ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
+lxc-unpriv-attach -n $name -- sh -c "echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config"
+lxc-unpriv-attach -n $name -- su pspp -c 'mkdir ~/.ssh'
+cat buildbot_rsa.pub | lxc-unpriv-attach -n $name -- /bin/bash -c "/bin/cat > /home/pspp/.ssh/authorized_keys"
 # pspp
-lxc-attach -n $name -- apt install -y build-essential python3 perl texinfo texlive \
+lxc-unpriv-attach -n $name -- apt install -y build-essential python3 perl texinfo texlive \
         libgsl-dev libgtk-3-dev libgtksourceview-3.0-dev \
         pkg-config gimp gperf git zip curl autoconf libtool \
         gettext libreadline-dev appstream \
-        texlive-plain-generic \
+        texlive-plain-generic librsvg2-bin \
         libtext-diff-perl
 lxc-stop $name
 
