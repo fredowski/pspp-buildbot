29f82f912a448644e92515208ead65da4ebb318f
Friedrich Beckmann <friedrich.beckmann@hs-augsburg.de>
2023-06-18 09:19:39 +0200

    update opensuse to gtksourceview4 - update to tumbleweed
    For the update to gtksourceview4 I updated from 15.2 to tumbleweed

--- create_suse_vm.sh
+++ create_suse_vm.sh
@@ -2,22 +2,23 @@
 
 name="opensuse"
 
-lxc-create -n $name -t download -f lxc-suse-config -- -d opensuse -r 15.2 -a amd64
+lxc-create -n $name -t download -f lxc-config -- --keyserver keyserver.ubuntu.com -d opensuse -r tumbleweed -a amd64
 systemd-run --user -p "Delegate=yes" lxc-start -F $name
 # wait for network
 sleep 15
-lxc-attach -n $name -- zypper install -y openssl openssh sudo
-lxc-attach -n $name -- /usr/sbin/useradd -s /bin/bash -m -p $(openssl passwd -1 pspp4you) pspp
-lxc-attach -n $name -- sh -c "echo 'pspp ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
-lxc-attach -n $name -- sh -c "echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config"
-lxc-attach -n $name -- su pspp -c 'mkdir ~/.ssh'
-cat buildbot_rsa.pub | lxc-attach -n $name -- /bin/bash -c "/bin/cat > /home/pspp/.ssh/authorized_keys"
+lxc-unpriv-attach -n $name -- zypper install -y openssl openssh sudo
+lxc-unpriv-attach -n $name -- /usr/sbin/useradd -s /bin/bash -m -p $(openssl passwd -1 pspp4you) pspp
+lxc-unpriv-attach -n $name -- sh -c "echo 'pspp ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
+lxc-unpriv-attach -n $name -- sh -c "echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config"
+lxc-unpriv-attach -n $name -- su pspp -c 'chmod a+rx ~'
+lxc-unpriv-attach -n $name -- su pspp -c 'mkdir ~/.ssh'
+cat buildbot_rsa.pub | lxc-unpriv-attach -n $name -- /bin/bash -c "/bin/cat > /home/pspp/.ssh/authorized_keys"
 # pspp
-lxc-attach -n $name -- zypper install -y gcc python3 perl texinfo texlive \
-        gsl-devel gtk3-devel gtksourceview-devel \
+lxc-unpriv-attach -n $name -- zypper install -y gcc python3 perl texinfo texlive \
+        gsl-devel gtk3-devel gtksourceview4-devel \
         pkg-config gperf git unzip curl autoconf libtool \
         texlive-wasy \
-        gettext-tools-mini readline-devel libxml2-devel automake make \
+        readline-devel libxml2-devel automake make \
         glibc-locale AppStream-devel
 lxc-stop $name
 
