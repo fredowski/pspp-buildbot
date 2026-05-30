5007d4b885df35a86e5f439d6219ae827d98c575
Friedrich Beckmann <friedrich.beckmann@hs-augsburg.de>
2026-05-31 00:51:18 +0200

    get_test_results: switch from scp to lxc-unpriv-attach + cat

--- get_test_results.sh
+++ get_test_results.sh
@@ -6,7 +6,4 @@ newname=$1
 
 lxc-info $newname
 pwd
-# Copy the testdir file from the container
-scp -o StrictHostKeyChecking=no \
-    -i /home/buildbot/pspp-buildbot/buildbot_rsa \
-    pspp@`lxc-info -n $newname -i -H`:/home/pspp/build/tests/testsuite.log .
+lxc-unpriv-attach -n $newname -- cat /home/pspp/build/tests/testsuite.log > testsuite.log
